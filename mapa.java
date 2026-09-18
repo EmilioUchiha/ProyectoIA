5ede5550a45f198bbf69aba3e931908b21fb7deb
Emilio Ramirez <emilioramirez1905@gmail.com>
2026-09-18 17:49:18 -0600

    Subo archivos vacios

---
+++ mapa.java
@@ -0,0 +1,7 @@
+public static void main(String[] args) {
+    Jugador jugador1 = new Jugador("Juan", 25, "Delantero");
+    Jugador jugador2 = new Jugador("Pedro", 30, "Defensa");
+
+    System.out.println(jugador1);
+    System.out.println(jugador2);
+}
